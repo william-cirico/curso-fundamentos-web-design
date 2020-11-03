2f93f0f24ca955525ae66297dfddc1b4cbb76008
William <contato.williamc@gmail.com>
2020-11-03 01:58:19 -0300

    Update

---
+++ secao-22-conjuntos-dados/205_arrays.js
@@ -0,0 +1,25 @@
+var frutas = ['Maçã', 'Banana'];
+
+// Último índice de um array
+var ultimo = frutas[frutas.length - 1];
+
+// Adicionar um item ao final do Array
+var adicionar = frutas.push('Laranja');
+
+// Remover um item do final do Array
+var ultimo = frutas.pop();
+
+// Remover do início do Array
+var primeiro = frutas.shift();
+
+// Adicionar ao início do Array
+var adicionar = frutas.unshift('Morango');
+
+// Procurar o índice de um item na Array
+var pos = frutas.indexOf('Banana')
+
+// Remover um item pela posição do índice
+var removedItem = fruta.splice(1, 1);
+
+// Copiar um Array
+var copiar = frutas.slice()
---
+++ secao-22-conjuntos-dados/211_objetos.js
@@ -0,0 +1,14 @@
+var pessoa = {
+    nome: {
+        primeiro: 'William',
+        sobrenome: 'Círico'
+    },
+    idade: 20,
+    sexo: 'Masculino'
+};
+
+console.log(pessoa.nome.primeiro);
+console.log(pessoa.nome.sobrenome);
+console.log(pessoa.idade);
+console.log(pessoa.idade);
+
